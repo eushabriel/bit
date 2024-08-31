c2b43563b6d6bdae52c41410eba5e91c61a28e67
Pau Herrera <pherrerael@gmail.com>
2024-08-31 18:35:34 +0800

    init

---
+++ auto.js
@@ -0,0 +1,6 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    for (let i = 0; i <= 6; i++) {
+        ns.run("hack.js");
+    }
+}
---
+++ hack.js
@@ -0,0 +1,18 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	let target = "n00dles";
+	let moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+
+	let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+	while(true) {
+			if (ns.getServerSecurityLevel(target) > securityThresh) {
+					await ns.weaken(target);
+			} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+					await ns.grow(target);
+			} else {
+					await ns.hack(target);
+			}
+		await ns.sleep(5000);
+	}
+}
