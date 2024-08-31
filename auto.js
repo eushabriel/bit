461215140616acbcd1aa59671fb408c9083aa8b6
Pau Herrera <pherrerael@gmail.com>
2024-08-31 19:31:18 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,7 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    let n = ns.getServerMaxRam / 2.65;
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i <= (ns.getServerMaxRam(ns.getHostname()) /  2.45); i++) {
         ns.run("hack.js");
     }
 }
